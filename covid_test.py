410bf920b67a6c0c7db9e2869f1cdedce15e9aab
Himanshu Singh Tomar <himanshusinghtomar204@gmail.com>
2021-05-05 19:50:42 +0530

    Add files via upload

---
+++ covid_test.py
@@ -0,0 +1,109 @@
+# covid-19 Symptom test!
+# This Program test Your Covid-19 according to WHO I.S.A Symptoms!
+# This Program is For Only and Only for Bihar!
+day = int(input("\033[1;32;40m Enter How many Day's you are ill:-\t"))
+if day == 1 or day == 2:
+    print("Are You felling common cold")
+    print("If you are felling these Problems")
+    ans = input("Enter Yes/Y or No/N\t").upper()
+    if ans == "YES" or ans == "Y":
+        print("\033[1;31;40m  Go To Check Up Immediately")
+        print("https://covidbihar.com/")
+        print("Go to this link immediately ")
+        print()
+    elif ans == "NO" or ans == "N":
+        print("Rest at Home for 3 Day's more")
+        print()
+        print("If you felt well in 4th Day then ok \n Otherwise Run This Program again")
+        print()
+    else:
+        print("Wrong Input !")
+        print("Don't Play this is a Serious Program")
+        print()
+elif day == 3:
+    print("Are You felling common cold/bit Throat pain")
+    print("Body temperature 98 Degree or 99 Degree")
+    print("Mild nausea , Vomiting and Mild diarrhea")
+    print("If you are felling these Problems")
+    ans = input("Enter Yes/Y or No/N\t").upper()
+    if ans == "YES" or ans == "Y":
+        print("\033[1;31;40mGo To Check Up Immediately")
+        print("https://covidbihar.com/")
+        print("Go to this link immediately ")
+        print()
+    elif ans == "NO" or ans == "N":
+        print("Rest at Home for 3 Day's more")
+        print()
+        print("If you felt well in 4th Day then ok \n Otherwise Run This Program again")
+        print()
+    else:
+        print("Wrong Input !")
+        print("Don't Play this is a Serious Program")
+        print()
+elif day == 4:
+    print("Are You felling common cold/Serious Throat pain")
+    print("Body temperature 98 Degree or 99 Degree")
+    print("Mild nausea , Vomiting and Mild diarrhea")
+    print("Felling weakness and joint pain")
+    print("If you are felling these Problems")
+    ans = input("Enter Yes/Y or No/N\t").upper()
+    if ans == "YES" or ans == "Y":
+        print("\033[1;31;40mGo To Check Up Immediately")
+        print("https://covidbihar.com/")
+        print("Go to this link immediately ")
+        print()
+    elif ans == "NO" or ans == "N":
+        print("Go To Check Up Immediately")
+        print("Rest at Home for 3 Day's more")
+        print()
+        print("If you felt well in 4th Day then ok \n Otherwise Run This Program again")
+        print()
+    else:
+        print("Wrong Input !")
+        print("Don't Play this is a Serious Program")
+        print()
+elif day == 5 or day == 6:
+    print("High Fever  and Dry cough / Serious Throat Pain")
+    print("Body temperature 100 Degree to 103 Degree")
+    print("Breath Difficulty")
+    print("Felling weakness and joint pain")
+    print("If you are felling these Problems")
+    ans = input("Enter Yes/Y or No/N\t").upper()
+    if ans == "YES" or ans == "Y":
+        print("\033[1;31;40mGo To Check Up Immediately")
+        print("https://covidbihar.com/")
+        print("Go to this link immediately ")
+        print()
+    elif ans == "NO" or ans == "N":
+        print("Rest at Home for 3 Day's more")
+        print()
+        print("If you felt well in 4th Day then ok \n Otherwise Run This Program again")
+        print()
+    else:
+        print("Wrong Input !")
+        print("Don't Play this is a Serious Program")
+        print()
+elif day == 7 or day == 8:
+    print("\033[1;31;40mGo To Check Up Immediately")
+    print("https://covidbihar.com/")
+    print("Go to this link immediately ")
+    print()
+else:
+    print("Wrong Input !")
+    print("Don't Play this is a Serious Program")
+    print()
+print("We Don't need too Panic During this Covid Situation ")
+print()
+print("The most common symptoms of COVID-19 are fever,tiredness, and dry cough. \n"
+      "Some patients may have aches and pains, nasal congestion,runny nose,\n "
+      "sore throat or diarrhea. These symptoms are usually mild and begin gradually.\n"
+      "Some people become infected but don’t develop any symptoms and don't feel unwell.\n "
+      "Most people(about 80%) recover from the disease without needing special treatment.\n"
+      "Around 1 out of every 6 people who gets COVID-19 becomes seriously ill and develops difficulty breathing.\n "
+      "Older people,and those with underlying medical problems like high blood pressure,heart problems or "
+      "diabetes,are more likely to develop serious illness. \n "
+      "People with fever, cough and difficulty breathing should seek medical attention. ")
+print()
+print("Go To This Link For Covid Help In Bihar")
+print("https://covidbihar.com/")
+
